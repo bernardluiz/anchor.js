1b02e5631cb87018ce7995266770baab7e6e95de
bernardluiz <135430056+bernardluiz@users.noreply.github.com>
2023-06-07 10:41:07 +0700

    Update anchor.js

--- anchor.js
+++ anchor.js
@@ -1,1 +1,9 @@
-asd
+<div style="display:none">
+  <a href="https://kelurahanlumpur.gresikkab.go.id/slot-pulsa-10-ribu/">slot pulsa</a>
+    <a href="https://bappeda.pacitankab.go.id/slot-dana/">slot dana</a>
+      <a href="https://kelurahanpekauman.gresikkab.go.id/slot-gacor-4d/">slot gacor</a>
+        <a href="https://kelurahantrate.gresikkab.go.id/data-sgp/">data sgp</a>
+  <a href="https://dpmd.lamandaukab.go.id/data-macau/">data macau</a>
+    <a href="https://slot-demo.ummetro.ac.id/">slot demo</a>
+      <a href="https://data-hk.kitsupervivencia.net/">data hk</a>
+</div>
